4eb403be1c17c31ae28dd875df7718a210bc473b
Arthur Redmond <19720427+ArthurRedmond@users.noreply.github.com>
2021-10-25 16:41:25 -0400

    Create functions in controller

---
+++ TacoCatMVC/Models/Palindrome.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TacoCatMVC.Models
+{
+    public class Palindrome
+    {
+        public string InputWord { get; set; }
+        public string RevWord { get; set; }
+        public bool IsPalendrome { get; set; }
+        public string Message { get; set; }
+    }
+}
